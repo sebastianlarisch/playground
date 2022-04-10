35002c50de98130049ac82cef840db54142038ec
TeufelSL <sebastian.larisch@teufel.de>
2022-04-10 20:57:53 +0200

    composer create-project

---
+++ playground/src/Test/TestClass.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace sebastianlarisch\PlayGround\Test;
+
+final class TestClass
+{
+    public function test(): void
+    {
+        echo "test";
+    }
+}
